355118386a42db3eb7ff0ee5c0d8e20dfd22e002
Tin Lai <oscar@tinyiu.com>
2021-03-21 03:01:24 +1100

    working dictionary
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- src/cpp2py_plotly/cpp2py_plotly.cpp
+++ src/cpp2py_plotly/cpp2py_plotly.cpp
@@ -5,7 +5,47 @@ namespace Plotly {
 ////////////////////////////////////////
 // implementation of Dictionary
 ////////////////////////////////////////
-Dictionary::Dictionary() { reset(); }
+
+void Dictionary::add_kwargs(const std::string &key, bool value) const {
+  (*m_msg->mutable_data())[key].set_bool_(value);
+}
+
+void Dictionary::add_kwargs(const std::string &key, double value) const {
+  (*m_msg->mutable_data())[key].set_double_(value);
+}
+
+void Dictionary::add_kwargs(const std::string &key, int value) const {
+  (*m_msg->mutable_data())[key].set_int_(value);
+}
+
+void Dictionary::add_kwargs(const std::string &key, const char *value) const {
+  (*m_msg->mutable_data())[key].set_string(value);
+}
+
+void Dictionary::add_kwargs(const std::string &key,
+                            const std::string &value) const {
+  (*m_msg->mutable_data())[key].set_string(value);
+}
+
+void Dictionary::add_kwargs(const std::string &key,
+                            const std::vector<double> &value) const {
+  (*m_msg->mutable_data())[key].set_allocated_series_d(
+      vec_to_allocated_seriesD(value));
+}
+
+void Dictionary::add_kwargs(const std::string &key,
+                            const std::vector<int> &value) const {
+  (*m_msg->mutable_data())[key].set_allocated_series_i(
+      vec_to_allocated_seriesI(value));
+}
+
+void Dictionary::add_kwargs(const std::string &key,
+                            Plotly::Dictionary &value) const {
+  (*m_msg->mutable_data())[key].set_allocated_dict(value.release_ptr());
+}
+void Dictionary::set_kwargs(Plotly::Dictionary &value) const {
+  m_msg->mutable_data()->swap(*value.m_msg->mutable_data());
+}
 
 void Dictionary::reset() { m_msg = std::make_unique<DictionaryMsg>(); }
 
@@ -14,21 +54,26 @@ DictionaryMsg *Dictionary::release_ptr() { return m_msg.release(); }
 ////////////////////////////////////////
 // implementation of Plotly Figure
 ////////////////////////////////////////
-Figure::Figure(std::string uuid) : uuid(std::move(uuid)) { reset(); }
+Figure::Figure(std::string uuid) : m_uuid(std::move(uuid)) { reset(); }
 
 void Figure::send(zmq::send_flags send_flags) {
   initialise_publisher();
+  // swap kwargs in the dictionary container with the protobuf internal msg
+  auto kwargs_ptr = m_kwargs.release_ptr();
+  m_msg.mutable_kwargs()->Swap(kwargs_ptr);
+  m_kwargs.m_msg = std::unique_ptr<DictionaryMsg>(kwargs_ptr);
+
   static_publisher->send(build_zmq_msg(), send_flags);
 }
 
 void Figure::reset() {
-  msg.Clear();
-  msg.set_uuid(uuid);
+  m_msg.Clear();
+  m_msg.set_uuid(m_uuid);
 }
 zmq::message_t Figure::build_zmq_msg() {
   // serial protobuf to string
   std::string encoded_msg;
-  msg.SerializeToString(&encoded_msg);
+  m_msg.SerializeToString(&encoded_msg);
   // memcpy to a zmq m_msg
   zmq::message_t zmq_msg(encoded_msg.size());
   memcpy((void *)zmq_msg.data(), encoded_msg.c_str(), encoded_msg.size());
@@ -52,39 +97,5 @@ PlotlyMsg::SeriesI *vec_to_allocated_seriesI(std::vector<int> value) {
   return series;
 }
 
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        bool value) {
-  (*msg)[key].set_bool_(value);
-}
-
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        double value) {
-  (*msg)[key].set_double_(value);
-}
-
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        int value) {
-  (*msg)[key].set_int_(value);
-}
-
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        const std::string &value) {
-  (*msg)[key].set_string(value);
-}
-
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        const std::vector<double> &value) {
-  (*msg)[key].set_allocated_series_d(vec_to_allocated_seriesD(value));
-}
-
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        const std::vector<int> &value) {
-  (*msg)[key].set_allocated_series_i(vec_to_allocated_seriesI(value));
-}
-
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        Plotly::Dictionary &value) {
-  (*msg)[key].set_allocated_dict(value.release_ptr());
-}
 //////////////////////////////////////////////////////////////////////////
 }; // namespace Plotly
--- src/cpp2py_plotly/cpp2py_plotly.hpp
+++ src/cpp2py_plotly/cpp2py_plotly.hpp
@@ -32,37 +32,362 @@ using DictionaryMsgData =
     google::protobuf::Map<std::string, PlotlyMsg::DictItemVal>;
 
 // Helpers
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        bool value);
+// void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
+//                        bool value);
+//
+// void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
+//                        double value);
+//
+// void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
+//                        int value);
+//
+// void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
+//                        const std::string &value);
+//
+// void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
+//                        const char *value);
+//
+// void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
+//                        const std::vector<double> &value);
+//
+// void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
+//                        const std::vector<int> &value);
+//
+// void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
+//                        Plotly::Dictionary &value);
 
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        double value);
+std::ostream &operator<<(std::ostream &out, DictionaryMsgData const &dict) {
+  out << "Dict(";
+  bool first_item = true;
+  for (auto &&it = dict.begin(); it != dict.end(); ++it) {
+    if (first_item)
+      first_item = false;
+    else
+      out << ", ";
 
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        int value);
+    out << it->first << ":";
+    auto itemVal = it->second;
 
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        const std::string &value);
-
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        const std::vector<double> &value);
-
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        const std::vector<int> &value);
-
-void dictmsg_add_kwargs(DictionaryMsgData *msg, const std::string &key,
-                        Plotly::Dictionary &value);
+    switch (itemVal.value_case()) {
+    case itemVal.kSeriesD:
+      out << "seriesD<..>";
+      break;
+    case itemVal.kSeriesI:
+      out << "seriesI<..>";
+      break;
+    case itemVal.kBool:
+      out << itemVal.bool_();
+      break;
+    case itemVal.kDict:
+      out << itemVal.dict().data();
+      break;
+    case itemVal.kDouble:
+      out << itemVal.double_();
+      break;
+    case itemVal.kInt:
+      out << itemVal.int_();
+      break;
+    case itemVal.kString:
+      out << itemVal.string();
+      break;
+    case itemVal.VALUE_NOT_SET:
+      break;
+    default:
+      throw std::runtime_error(
+          "Unimplemented DictItemVal " +
+          std::to_string(static_cast<int>(itemVal.value_case())));
+    };
+  }
+  out << ")";
+  return out;
+}
 
 // class that represents dict representation in protobuf
 class Dictionary {
 
 public:
-  Dictionary();
+  //  template <typename T, typename... Types>
+  //  static Dictionary get_dict(const std::string &key, T val) {
+  //    add_kwargs(key, val);
+  //  }
+  //
+  //  template <typename T>
+  //  static Dictionary get_dict(const std::string &key, T val) {
+  //    Dictionary dict;
+  //    dict.add_kwargs(key, val);
+  //  }
 
-  template <typename T> void add_kwargs(std::string key, const T &value) {
-    dictmsg_add_kwargs(m_msg->mutable_data(), key, value);
+  Dictionary() { reset(); }
+
+
+//  template <typename Types>
+//      void _constructor_helper(const std::string &key, T val) {
+//      add_kwargs(key, val);
+//    }
+
+
+
+
+
+
+
+
+
+
+//  template <typename T, typename Args>
+//  void aa(std::string key, T val, Args args...) {
+////      add_kwargs(key, val);
+//  }
+//
+//  template <typename T>
+//  void aa(std::string key, T val) {
+////      add_kwargs(key, val);
+//  }
+//
+//
+////  template <typename Types>
+//  template <typename T, typename Args
+//
+////            ,
+////            class = typename std::enable_if
+////            <
+////                !std::is_lvalue_reference<T>::value
+////            >::type
+//           >
+//  Dictionary(T &&val, Args args...){
+//      T lval(val);
+//      aa(lval, args);
+//  }
+//
+//
+
+
+
+
+template< class T >
+using decay_t = typename std::decay<T>::type;
+
+//
+//  //  template <typename T>
+//  template <typename T, typename Types>
+//  Dictionary(const std::string &key, T val ,Types rest...)
+//      : Dictionary(rest) {
+//    add_kwargs(key, val);
+//  }
+//  template <typename T>
+//  Dictionary(const std::string &key, T val)
+//      : Dictionary() {
+//    add_kwargs(key, val);
+//  }
+
+
+  //  template <typename T>
+  template <typename T, typename... Types>
+  Dictionary(const std::string &key, T val , Types ...rest)
+      : Dictionary(rest...) {
+    add_kwargs(key, val);
   }
 
+  template <typename T>
+  Dictionary(const std::string &key, T val)
+      : Dictionary() {
+    add_kwargs(key, val);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////  template <typename Types>
+//  template <typename T, typename Args,
+//            class = typename std::enable_if
+//            <
+//                std::is_lvalue_reference<T>::value
+//            >::type
+//           >
+//  Dictionary(T val, Args args...){
+////      aa(val, args);
+//  }
+
+
+
+
+
+
+  // copy-construct
+  Dictionary(Dictionary &dict) {
+    reset();
+    m_msg.swap(dict.m_msg);
+  }
+
+  // rvalue-construct
+  Dictionary(Dictionary &&dict) {
+    m_msg = std::move(dict.m_msg);
+  }
+
+  /*
+
+
+
+  //
+  //  Dictionary(const Dictionary &dict) {
+  //    reset();
+  //    m_msg.swap(dict.m_msg);
+  //  }
+
+  //  Dictionary(Plotly::Dictionary &dict) {
+  //    throw std::runtime_error("no");
+  //  }
+
+  //  Dictionary(Plotly::Dictionary &dict) {
+  //    m_msg.swap(dict.m_msg);
+  //  }
+
+  //  Dictionary(const Plotly::Dictionary dict) {
+  //    m_msg = std::unique_ptr<DictionaryMsg>(dict.m_msg);
+  //  }
+
+  template <typename T>
+  Dictionary(const std::string &key, T val) : Dictionary() {
+    _constructor_helper(key, val);
+  }
+
+  //  // rvalue as argument
+  //  Dictionary(const std::string &key, Dictionary &&val) : Dictionary() {
+  //    Dictionary lval(val);
+  //    add_kwargs(key, lval);
+  //  }
+
+  // return invert_x< std::remove_reference_t<P> >( pin,
+  //                                                std::is_lvalue_reference<P&&>());
+  // return invert_x< std::remove_reference_t<P> >( pin,
+  //                                                std::is_lvalue_reference<P&&>());
+
+  // template<typename T, typename Types,
+  //            std::enable_if_t<!std::is_rvalue_reference<T
+  //     >::value, int> = 0>
+
+  //  template <typename T,
+  //  std::enable_if_t<!std::is_rvalue_reference<Dictionary
+  //     >::value, int> = 0>
+
+  //  template <typename T,
+  //  std::enable_if_t<!std::is_rvalue_reference<Dictionary
+  //     >::value, int> = 0>
+  //  void _constructor_helper(const std::string &key, T val) {
+  //    add_kwargs(key, val);
+  //  }
+
+  void _constructor_helper(const std::string &key, Dictionary &val) {
+    Dictionary lval(val);
+    add_kwargs(key, lval);
+  }
+
+//  template <typename T>
+  template <typename T, typename Types,
+            class = typename std::enable_if
+            <
+                !std::is_lvalue_reference<T>::value
+            >::type
+           >
+  Dictionary(
+      const std::string &key, T &&val
+//      const std::string &key1, T &&val1
+             ,Types rest...
+             )
+      : Dictionary() {
+
+    //    Dictionary lval(val);
+    add_kwargs(key, val);
+
+    //    Dictionary lval(val);
+    _constructor_helper(key, val);
+    //    _constructor_helper(key, Dictionary());
+    //    _constructor_helper(key, lval);
+  }
+
+
+
+
+
+  */
+
+
+
+
+
+
+
+
+
+  //  template <typename T, typename... Types>
+  //  Dictionary(const std::string &key, Dictionary && val, Types... rest)
+  //      : Dictionary(rest...) {
+  //    _constructor_helper(key, val);
+  //
+  //  }
+
+  //
+  //  // rvalue as argument
+  //  template <typename... Types>
+  //  Dictionary(const std::string &key, Dictionary &&val, Types... rest)
+  //      : Dictionary(rest...) {
+  //    Dictionary lval(val);
+  //    add_kwargs(key, lval);
+  //  }
+
+  //  template <typename T, typename... Types>
+  //  Dictionary(const std::string &key, T val, Types... rest)
+  //      : Dictionary(rest...) {
+  //    add_kwargs(key, val);
+  //  }
+
+  //
+  //  template <typename T, typename... Types>
+  //  Dictionary(const std::string &key, Dictionary &&val, Types... rest)
+  //      : Dictionary(rest...) {
+  //    add_kwargs(key, val);
+  //  }
+
+  //  template <typename T, typename... Types>
+  //  Dictionary(std::string key, Dictionary val, Types... rest) :
+  //  Dictionary(rest...) {
+  //    add_kwargs(key, val);
+  //  }
+
+  void add_kwargs(const std::string &key, bool value) const;
+
+  void add_kwargs(const std::string &key, double value) const;
+
+  void add_kwargs(const std::string &key, int value) const;
+
+  void add_kwargs(const std::string &key, const std::string &value) const;
+
+  void add_kwargs(const std::string &key, const char *value) const;
+
+  void add_kwargs(const std::string &key,
+                  const std::vector<double> &value) const;
+
+  void add_kwargs(const std::string &key, const std::vector<int> &value) const;
+
+  void add_kwargs(const std::string &key, Plotly::Dictionary &value) const;
+
+  void set_kwargs(Plotly::Dictionary &value) const;
+
+  friend std::ostream &operator<<(std::ostream &out, Dictionary const &dict) {
+    return out << (*dict.m_msg).data();
+  }
   void reset();
 
   DictionaryMsg *release_ptr();
@@ -71,17 +396,69 @@ public:
   std::unique_ptr<DictionaryMsg> m_msg;
 };
 
+////////////////////////////////////////
+// Dictionary Builder
+////////////////////////////////////////
+
+template <typename T>
+static void _get_dict_helper(const Dictionary &dict, const std::string &key,
+                             T val) {
+  dict.add_kwargs(key, val);
+}
+
+template <typename T, typename... Types>
+static void _get_dict_helper(const Dictionary &dict, const std::string &key,
+                             T val, Types... rest) {
+  dict.add_kwargs(key, val);
+  _get_dict_helper(dict, rest...);
+}
+
+template <typename T, typename... Types>
+static void _get_dict_helper(const Dictionary &dict, const std::string &key,
+                             Dictionary val, Types... rest) {
+  dict.add_kwargs(key, val);
+  _get_dict_helper(dict, rest...);
+}
+
+template <typename... Args> static Dictionary get_dict(Args... args) {
+  Dictionary dict;
+  _get_dict_helper(dict, args...);
+  return dict;
+}
+
+////////////////////////////////////////
+
 class Figure {
 public:
   explicit Figure(std::string uuid = "default");
 
-  template <typename T>
-  void add_kwargs(const std::string &key, const T &value) {
-    dictmsg_add_kwargs(msg.mutable_kwargs()->mutable_data(), key, value);
+  void set_uuid(const std::string &_uuid) {
+    m_uuid = _uuid;
+    m_msg.set_uuid(m_uuid);
   }
 
+  //  template <typename T>
+  //  void add_kwargs(const std::string &key, const T &value) {
+  //    dictmsg_add_kwargs(msg.mutable_kwargs()->mutable_data(), key, value);
+  //  }
+
   template <typename T> void add_kwargs(const std::string &key, T &value) {
-    dictmsg_add_kwargs(msg.mutable_kwargs()->mutable_data(), key, value);
+    //    dictmsg_add_kwargs(msg.mutable_kwargs()->mutable_data(), key, value);
+    m_kwargs.add_kwargs(key, value);
+  }
+
+  void set_kwargs(Dictionary &value) {
+    m_kwargs.set_kwargs(value);
+  }
+
+  // by r-value
+  void set_kwargs(Dictionary &&value) {
+    Dictionary lval(value);
+    m_kwargs.set_kwargs(lval);
+  }
+
+  const Dictionary get_kwargs() {
+    return m_kwargs;
   }
 
   void send(zmq::send_flags send_flags = zmq::send_flags::dontwait);
@@ -92,7 +469,16 @@ private:
   zmq::message_t build_zmq_msg();
 
   // variables
-  PlotlyMsg::Figure msg;
-  std::string uuid;
+  PlotlyMsg::Figure m_msg;
+  std::string m_uuid;
+  Dictionary m_kwargs;
 };
+
+////////////////////////////////////////
+// Helpers
+////////////////////////////////////////
+PlotlyMsg::SeriesD *vec_to_allocated_seriesD(std::vector<double> value);
+
+PlotlyMsg::SeriesI *vec_to_allocated_seriesI(std::vector<int> value);
+
 } // namespace Plotly
